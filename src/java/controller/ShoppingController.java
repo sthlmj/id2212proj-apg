fe945d75ad0f5f47fa2b4b416cd26e306634654c
guuurris <wiiala.gustav@gmail.com>
2015-12-14 17:00:50 +0100

    bla bla

--- src/java/controller/ShoppingController.java
+++ src/java/controller/ShoppingController.java
@@ -5,24 +5,87 @@
  */
 package controller;
 
+import java.util.List;
+import javax.annotation.Resource;
+import javax.ejb.SessionContext;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.transaction.UserTransaction;
+import model.Product;
 
 /**
  *
  * @author joehulden
  */
+
 @Stateless
 public class ShoppingController {
 
-     @PersistenceContext(unitName = "id2212proj-apgPU")
+    @PersistenceContext(unitName = "id2212proj-apgPU")
     private EntityManager em;
+    
+    @Resource
+    private SessionContext context;
 
+    @Resource
+    private UserTransaction usertransaction;
+    
     //Manages Entity
     public EntityManager getEntityManager() {
         return em;
     }
     
+    /**
+     * 
+     * @param type - product type
+     * @param units - number of products
+     * @return Product if match null else
+     */
+    public Product takeProduct(String type, int units){
+        try{
+           return (Product) em.createQuery("SELECT p FROM Product p WHERE type=:type AND unit <=:value").setParameter("type", units).setParameter("value", units).getResultList().get(0);
+        } catch (Exception e){
+            return null;
+ 
+        }
+    }
+    
+    /**
+     * This method demands that we have an open transaction 
+     * so that products can be bought
+     * @return if products was bought or not
+     */
+    
+    public boolean buy(List<Product> list){
+       for(Product p : list){
+           
+           Product onMarketP = (Product) em.createQuery("SELECT p FROM Product p WHERE unit <=:value").setParameter("type", p.getId()).setParameter("value", p.getUnits()).getResultList().get(0);
+           
+           //we buy a part of all the products
+           if (onMarketP == null){
+               context.getRollbackOnly();
+               return false;
+           }
+           //If we buy the last products on the market
+           if(onMarketP.getUnits() == p.getUnits()){
+               em.remove(onMarketP);
+           }
+           else { // uppdate product information
+               em.merge(new Product(p.getId(),onMarketP.getUnits() - p.getUnits()));
+           }
+          
+       }
+        
+        return true;
+    }
+    
+    /**
+     * The full product list
+     * @return - a list of all the products
+     */
+    public List<Product> listProducts(){
+      return em.createQuery("SELECT p FROM Product p").getResultList();     
+    }
     
 }
--- src/java/view/CustomerView.java
+++ src/java/view/CustomerView.java
@@ -6,13 +6,16 @@
 package view;
 
 import controller.CustomerController;
+import controller.ShoppingController;
 import java.io.Serializable;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.SessionScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.validator.ValidatorException;
 import javax.inject.Named;
+import model.Product;
 
 /**
  *
@@ -24,8 +27,43 @@ public class CustomerView implements Serializable{
     @EJB
     private CustomerController cont;
     
+    @EJB
+    private ShoppingController cartCont;
+    
+    //Register and login credential
     private String username;
     private boolean signedIn;
+    
+    //Shop information
+    private String type;
+    private int units; 
+
+    private List<Product> cart;
+
+    public List<Product> getCart() {
+        return cart;
+    }
+
+    public void setCart(List<Product> cart) {
+ 
+        this.cart = cart;
+    }
+    
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getUnits() {
+        return units;
+    }
+
+    public void setUnits(int units) {
+        this.units = units;
+    }
 
     
     public boolean isSignedIn() {
@@ -74,4 +112,15 @@ public class CustomerView implements Serializable{
         FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
     }
     
+    public Product takeProduct(){
+        return cartCont.takeProduct(type, units);
+    }
+    
+    public boolean checkoutcart(){
+        return cartCont.buy(cart);
+    }
+    
+    public List<Product> allProducts(){
+        return cartCont.listProducts();
+    }
 }
